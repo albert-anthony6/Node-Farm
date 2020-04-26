3ac152e4ba743dae9009310e851b08d97d9d9166
Albert Anthony <albert.anthony1999@gmail.com>
2020-04-26 03:50:33 -0500

    Fixed back button href in product page

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ const data = fs.readFileSync('./starter/dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview Page
-    if(pathName === '/overview' || pathName === '/'){
+    if(pathname === '/overview' || pathname === '/'){
         res.writeHead(200, { "Content-type": 'text/html' });
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -57,11 +57,15 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product Page
-    } else if (pathName === '/product') {
-        res.end('This is the product');
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product);
 
+        res.end(output);
+        
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(JSON.stringify(data));
 
